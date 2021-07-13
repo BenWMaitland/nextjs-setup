63e2840da3c2111a1f5e85267e7f323bf28f6e81
Ben <benwaltonmaitland@gmail.com>
2021-07-13 09:02:32 -0400

    add locale buttons

--- pages/landingPage/index.js
+++ pages/landingPage/index.js
@@ -1,14 +1,19 @@
 import React, { useContext, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Context from "../../store/context"; // globalState
-
 import { useTranslation } from 'next-i18next' // i18n
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'  // i18n
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'; // i18n
+import { Button } from '@material-ui/core';
+import Link from 'next/link' // nextjs
 
 const useStyles = makeStyles((theme) => ({
     landingPageContainer: {
         backgroundColor: theme.palette.primary.main,
+    },
+    button: {
+        backgroundColor: "blue",
+        margin: 20,
     },
 }));
 
@@ -26,16 +31,35 @@ export default function LandingPage() {
 
     return (
         <div className={classes.landingPageContainer}>
-            <h1>{t('header')}</h1>
+            <h1>
+                {t('header')} {/* inserts translation for text with this key */}
+            </h1>
             <p>
                 {globalState.displayText}
             </p>
+            <Link
+                href={{ pathname: router.pathname }}
+                locale={router.locale = 'en'}>
+                <Button
+                    className={classes.button}>
+                    EN
+                </Button>
+            </Link>
+
+            <Link
+                href={{ pathname: router.pathname }}
+                locale={router.locale = 'fr'}>
+                <Button
+                    className={classes.button}>
+                    FR
+                </Button>
+            </Link>
         </div>
     )
 }
 
 export const getStaticProps = async ({ locale }) => ({
     props: {
-        ...await serverSideTranslations(locale, ['landingpage']),
+        ...await serverSideTranslations(locale, ['landingpage']), // page must wait for this translation file to load
     },
 })
